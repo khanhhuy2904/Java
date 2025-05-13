f336c4d944ec0a63de9a0febfbcdc4195d2ac0a9
khanhhuy2904 <152388886+khanhhuy2904@users.noreply.github.com>
2025-05-13 15:15:40 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,30 +8,30 @@ public class Main {
         System.out.println("Nhập số n: ");
         int n = sc.nextInt();
         //2
-//        if(isPrime(n)){
-//            System.out.println("Yes");
-//        }
-//        else {
-//            System.out.println("No");
-//        }
-//        //3
-//        for(int i=0; i<=n;i++){
-//            if(isPrime(i)){
-//                System.out.println(i);
-//            }
-//        }
-//        //4
-//        int a = 0, b = 1;
-//        for(int i = 0; i<n; i++){
-//            System.out.print(a);
-//            if(i<n-1){
-//                System.out.print(", ");
-//            }
-//            int next = a + b;
-//            a = b;
-//            b = next;
-//        }
-//        System.out.println(" ");
+        if(isPrime(n)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        //3
+        for(int i=0; i<=n;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+        //4 Fibonacci
+        int a = 0, b = 1;
+        for(int i = 0; i<n; i++){
+            System.out.print(a);
+            if(i<n-1){
+                System.out.print(", ");
+            }
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println(" ");
 
         //5
         System.out.println("VD1:");
@@ -67,19 +67,14 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
+        if (n < 2 || (n % 2 == 0 && n != 2)) return false;
 
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        for (int i = 3; i * i <= n; i += 2) {
             if (n % i == 0) return false;
         }
         return true;
     }
 
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+
+
 }
